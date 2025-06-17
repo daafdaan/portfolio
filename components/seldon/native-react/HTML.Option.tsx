58fc3c3f9a80649ca12b7679d2c50c8ec1455cd9
David Notté <subscriptions@daafdaan.com>
2025-06-17 11:10:52 +0200

    Update HTML.Option.tsx

---
+++ components/seldon/native-react/HTML.Option.tsx
@@ -0,0 +1,10 @@
+/**
+ * This code was generated using Seldon (https://seldon.app)
+ * Licensed under the Terms of Use: https://seldon.app/terms
+ * Do not redistribute or sublicense without permission.
+ */
+import { OptionHTMLAttributes } from "react"
+
+export const HTMLOption = (props: OptionHTMLAttributes<HTMLOptionElement>) => {
+  return <option {...props} />
+}
