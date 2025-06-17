b097b87d079753feb042bf63d9393168484b1c15
David Notté <subscriptions@daafdaan.com>
2025-06-17 11:10:53 +0200

    Update HTML.Select.tsx

---
+++ components/seldon/native-react/HTML.Select.tsx
@@ -0,0 +1,10 @@
+/**
+ * This code was generated using Seldon (https://seldon.app)
+ * Licensed under the Terms of Use: https://seldon.app/terms
+ * Do not redistribute or sublicense without permission.
+ */
+import { SelectHTMLAttributes } from "react"
+
+export const HTMLSelect = (props: SelectHTMLAttributes<HTMLElement>) => {
+  return <select {...props} />
+}
