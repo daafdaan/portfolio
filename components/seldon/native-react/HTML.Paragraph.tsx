5edf3382568ba21e4e3f99fbf3a4d66e421aa27a
David Notté <subscriptions@daafdaan.com>
2025-06-17 11:10:53 +0200

    Update HTML.Paragraph.tsx

---
+++ components/seldon/native-react/HTML.Paragraph.tsx
@@ -0,0 +1,10 @@
+/**
+ * This code was generated using Seldon (https://seldon.app)
+ * Licensed under the Terms of Use: https://seldon.app/terms
+ * Do not redistribute or sublicense without permission.
+ */
+import { HTMLAttributes } from "react"
+
+export const HTMLParagraph = (props: HTMLAttributes<HTMLParagraphElement>) => {
+  return <p {...props} />
+}
