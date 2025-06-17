8f235a779e7ff2d3f7d8ebb7639d4dc21306a290
David Notté <subscriptions@daafdaan.com>
2025-06-17 11:10:59 +0200

    Update HTML.Textarea.tsx

---
+++ components/seldon/native-react/HTML.Textarea.tsx
@@ -0,0 +1,12 @@
+/**
+ * This code was generated using Seldon (https://seldon.app)
+ * Licensed under the Terms of Use: https://seldon.app/terms
+ * Do not redistribute or sublicense without permission.
+ */
+import { TextareaHTMLAttributes } from "react"
+
+export const HTMLTextarea = (
+  props: TextareaHTMLAttributes<HTMLTextAreaElement>,
+) => {
+  return <textarea {...props} />
+}
