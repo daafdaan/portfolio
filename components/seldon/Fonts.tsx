68aefb183579221929f685767b8f3ab441a12151
David Notté <subscriptions@daafdaan.com>
2025-06-17 11:11:08 +0200

    Update Fonts.tsx

---
+++ components/seldon/Fonts.tsx
@@ -0,0 +1,21 @@
+/**
+ * This code was generated using Seldon (https://seldon.app)
+ * Licensed under the Terms of Use: https://seldon.app/terms
+ * Do not redistribute or sublicense without permission.
+ */
+export function Fonts() {
+  return (
+    <>
+      <link
+        key="Inter"
+        href="https://fonts.googleapis.com/css2?family=Inter%3Aital%2Cwght%400%2C100%3B0%2C200%3B0%2C300%3B0%2C400%3B0%2C500%3B0%2C600%3B0%2C700%3B0%2C800%3B0%2C900%3B1%2C100%3B1%2C200%3B1%2C300%3B1%2C400%3B1%2C500%3B1%2C600%3B1%2C700%3B1%2C800%3B1%2C900&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        key="IBM Plex Sans"
+        href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans%3Aital%2Cwght%400%2C100%3B0%2C200%3B0%2C300%3B0%2C400%3B0%2C500%3B0%2C600%3B0%2C700%3B0%2C800%3B0%2C900%3B1%2C100%3B1%2C200%3B1%2C300%3B1%2C400%3B1%2C500%3B1%2C600%3B1%2C700%3B1%2C800%3B1%2C900&display=swap"
+        rel="stylesheet"
+      />
+    </>
+  )
+}
