70fdb25f0d238b1e444f7b32a58086ee45464906
David Notté <subscriptions@daafdaan.com>
2025-06-17 11:10:59 +0200

    Update HTML.Td.tsx

---
+++ components/seldon/native-react/HTML.Td.tsx
@@ -0,0 +1,13 @@
+/**
+ * This code was generated using Seldon (https://seldon.app)
+ * Licensed under the Terms of Use: https://seldon.app/terms
+ * Do not redistribute or sublicense without permission.
+ */
+import { TdHTMLAttributes } from "react"
+
+export const HTMLTd = ({
+  style,
+  ...props
+}: TdHTMLAttributes<HTMLTableCellElement>) => {
+  return <td style={{ ...style }} {...props} />
+}
