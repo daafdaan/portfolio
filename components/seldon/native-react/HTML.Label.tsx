de3dbfef97a0b74710635ab74429cb32908ad975
David Notté <subscriptions@daafdaan.com>
2025-06-17 11:10:46 +0200

    Update HTML.Label.tsx

---
+++ components/seldon/native-react/HTML.Label.tsx
@@ -0,0 +1,10 @@
+/**
+ * This code was generated using Seldon (https://seldon.app)
+ * Licensed under the Terms of Use: https://seldon.app/terms
+ * Do not redistribute or sublicense without permission.
+ */
+import { LabelHTMLAttributes } from "react"
+
+export const HTMLLabel = (props: LabelHTMLAttributes<HTMLLabelElement>) => {
+  return <label {...props} />
+}
