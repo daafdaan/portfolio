738d9fa213d0235fa98af99b47ff5b17afd417d4
David Notté <subscriptions@daafdaan.com>
2025-06-17 11:10:44 +0200

    Update HTML.Img.tsx

---
+++ components/seldon/native-react/HTML.Img.tsx
@@ -0,0 +1,10 @@
+/**
+ * This code was generated using Seldon (https://seldon.app)
+ * Licensed under the Terms of Use: https://seldon.app/terms
+ * Do not redistribute or sublicense without permission.
+ */
+import { ImgHTMLAttributes } from "react"
+
+export const HTMLImg = (props: ImgHTMLAttributes<HTMLImageElement>) => {
+  return <img {...props} />
+}
