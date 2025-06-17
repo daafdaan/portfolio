69459327c8c0e9d683a4dd3174611f4d1625175e
David Notté <subscriptions@daafdaan.com>
2025-06-17 11:10:45 +0200

    Update HTML.Input.tsx

---
+++ components/seldon/native-react/HTML.Input.tsx
@@ -0,0 +1,10 @@
+/**
+ * This code was generated using Seldon (https://seldon.app)
+ * Licensed under the Terms of Use: https://seldon.app/terms
+ * Do not redistribute or sublicense without permission.
+ */
+import { InputHTMLAttributes } from "react"
+
+export const HTMLInput = (props: InputHTMLAttributes<HTMLInputElement>) => {
+  return <input {...props} />
+}
